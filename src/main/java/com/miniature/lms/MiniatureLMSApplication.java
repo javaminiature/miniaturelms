57a36a795a42d7d47a3fc8c8c68ba5cf271ddef5
Birla Yogapalraj <birla.yogapalraj@sap.com>
2022-09-17 18:07:39 -0400

    Revert "added open api docs"
    This reverts commit c0979090

--- src/main/java/com/miniature/lms/MiniatureLMSApplication.java
+++ src/main/java/com/miniature/lms/MiniatureLMSApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.miniature.lms"})
 @EnableAutoConfiguration
-@OpenAPIDefinition(info = @Info(title = "Miniature LMS APIs", version = "1.0", description = "LMS API Information"))
 public class MiniatureLMSApplication {
 	public static void main(String args[]) {
 		SpringApplication.run(MiniatureLMSApplication.class,args);
