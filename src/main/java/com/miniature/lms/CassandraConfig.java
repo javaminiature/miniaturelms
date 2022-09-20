1eb25a58dbc71f7313ea519aaa9a67ea5b681b86
Birla Yogapalraj <birla.yogapalraj@sap.com>
2022-09-20 18:29:49 -0400

    added consul config centralised management

--- src/main/java/com/miniature/lms/CassandraConfig.java
+++ src/main/java/com/miniature/lms/CassandraConfig.java
@@ -1,6 +1,8 @@
 package com.miniature.lms;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
